d047b322b097858c3e78dedc217c50048e030c7e
Pawel Kedziora <pakedziora@gmail.com>
2019-04-25 08:03:05 +0100

    100 epochs

--- train_model.py
+++ train_model.py
@@ -63,7 +63,7 @@ callbacks_list = [checkpoint]
 history = model.fit_generator(
       train_generator,
       steps_per_epoch=100,
-      epochs=2,
+      epochs=100,
       validation_data=validation_generator,
       validation_steps=50,
       callbacks=callbacks_list,
