55378ae51fb9c2d2075d6d6168381486dc14bd8f
Pawel Kedziora <pakedziora@gmail.com>
2019-04-27 14:29:10 +0100

    Cleanup command line script

--- cmd.py
+++ cmd.py
@@ -1,6 +1,12 @@
+import sys
+
+if len(sys.argv) < 2:
+    print(f"Missing image name")
+    quit()
+
 from recognizer import *
 
-filePath = "/home/pakedziora/Downloads/006.jpg"
+filePath = sys.argv[1]
 
 islavalamp = recognize(filePath)
 
