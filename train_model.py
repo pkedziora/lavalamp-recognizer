cbdbdb36b7df9def1f80310ebd59dd3352ea86fd
Pawel Kedziora <pakedziora@gmail.com>
2019-05-04 09:11:12 +0200

    Comments

--- train_model.py
+++ train_model.py
@@ -16,8 +16,7 @@ conv_base = VGG16(weights='imagenet',
                   include_top=False,
                   input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
 
-
-#Build model
+# Build model
 model = models.Sequential()
 model.add(conv_base)
 model.add(layers.Flatten())
@@ -28,15 +27,15 @@ model.summary()
 
 conv_base.trainable = False
 
-#Compile model
+# Compile model
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=2e-5),
               metrics=['acc'])
 
-#Create data generators
+# Create data generators
 train_dir = 'data/train'
 train_imagegenerator = ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
     rotation_range=30,
     width_shift_range=0.1,
     height_shift_range=0.1,
@@ -45,45 +44,46 @@ train_imagegenerator = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode='nearest')
 train_generator = train_imagegenerator.flow_from_directory(
-        train_dir,
-        target_size=(IMAGE_SIZE, IMAGE_SIZE),
-        batch_size=10,
-        class_mode='binary')
+    train_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=10,
+    class_mode='binary')
 
 validation_dir = 'data/validation'
-validation_imagegenerator = ImageDataGenerator(rescale=1./255)
+validation_imagegenerator = ImageDataGenerator(rescale=1. / 255)
 validation_generator = validation_imagegenerator.flow_from_directory(
-        validation_dir,
-        target_size=(IMAGE_SIZE, IMAGE_SIZE),
-        batch_size=10,
-        class_mode='binary')
+    validation_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=10,
+    class_mode='binary')
 
-checkpoint = ModelCheckpoint(MODEL_FILENAME, monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='max')
+checkpoint = ModelCheckpoint(MODEL_FILENAME, monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False,
+                             mode='max')
 callbacks_list = [checkpoint]
 
 class_weights = class_weight.compute_class_weight('balanced',
-                                                 np.unique(train_generator.classes),
-                                                 train_generator.classes)
+                                                  np.unique(train_generator.classes),
+                                                  train_generator.classes)
 
-#train model
+# Train model
 history = model.fit_generator(
-      train_generator,
-      steps_per_epoch=1320,
-      epochs=100,
-      validation_data=validation_generator,
-      validation_steps=320,
-      callbacks=callbacks_list,
-      class_weight = class_weights,
-      workers=8)
+    train_generator,
+    steps_per_epoch=1320,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=320,
+    callbacks=callbacks_list,
+    class_weight=class_weights,
+    workers=8)
 
-#test model
+# Test model
 test_dir = 'data/test'
-test_imagegenerator = ImageDataGenerator(rescale=1./255)
+test_imagegenerator = ImageDataGenerator(rescale=1. / 255)
 test_generator = test_imagegenerator.flow_from_directory(
-        test_dir,
-        target_size=(IMAGE_SIZE, IMAGE_SIZE),
-        batch_size=10,
-        class_mode='binary')
+    test_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=10,
+    class_mode='binary')
 
 test_loss, test_acc = model.evaluate_generator(test_generator, steps=320, verbose=1)
 print('test accuracy:', test_acc)
